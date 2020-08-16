1b709a712e26f45bb6c34f9186c6daae0e4f3dd4
pkumaravel <p.kumaravelin@gmail.com>
2020-08-16 03:06:25 -0400

    Updates

---
+++ precache-manifest.9e74d4cf709c3623594a1a704f5eac33.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ba54d4bb3e98d7f3e5c00450b2b1990",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "ffc3aea6e9aeadfd8054",
+    "url": "/monster-rolodex/static/css/main.c1d82dd6.chunk.css"
+  },
+  {
+    "revision": "cfceac3283c0ca996338",
+    "url": "/monster-rolodex/static/js/2.9c424b6b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.9c424b6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ffc3aea6e9aeadfd8054",
+    "url": "/monster-rolodex/static/js/main.d7ea202e.chunk.js"
+  },
+  {
+    "revision": "bfd22c638cbcd931c63c",
+    "url": "/monster-rolodex/static/js/runtime-main.0bb500a1.js"
+  }
+]);
